fb8f91293e290cc3d63fb63c1f48c94af1cf89bd
fwoggydweller <asanch42@ucm.es>
2024-10-09 17:30:39 +0200

    First level

--- src/tp1/logic/Game.java
+++ src/tp1/logic/Game.java
@@ -10,8 +10,8 @@ public class Game {
 
 	public static final int DIM_X = 10; 
 	public static final int DIM_Y = 10;
-	public static final int INITIAL_LEMMING_NUM = 1;
-	public static final int NUMBER_OF_WALLS = 10;
+	public static final int INITIAL_LEMMING_NUM = 4;
+	public static final int NUMBER_OF_WALLS = 15;
 	private int LEMMING_EXIT = 0; 
 	private GameObjectContainer cont;
 	private WalkerRole w = new WalkerRole();
@@ -20,9 +20,26 @@ public class Game {
 	public Game(int nLevel) {
 		cont = new GameObjectContainer();
 		if(nLevel == 1) { //adds 1 of each type	
-			cont.registerDoor(new ExitDoor(9,9));
-			cont.addLemming(new Lemming(1, 1, w, this));
-			cont.addWall(new Wall(1,2));		
+			cont.registerDoor(new ExitDoor(4,5));
+			cont.addLemming(new Lemming(9, 0, w, this));
+			cont.addLemming(new Lemming(3, 3, w, this));
+			cont.addLemming(new Lemming(2, 3, w, this));
+			cont.addLemming(new Lemming(0, 8, w, this));
+			cont.addWall(new Wall(9,1));	
+			cont.addWall(new Wall(8,1));	
+			cont.addWall(new Wall(2,4));
+			cont.addWall(new Wall(3,4));	
+			cont.addWall(new Wall(4,4));	
+			cont.addWall(new Wall(7,5));
+			cont.addWall(new Wall(7,6));	
+			cont.addWall(new Wall(6,6));	
+			cont.addWall(new Wall(5,6));
+			cont.addWall(new Wall(4,6));	
+			cont.addWall(new Wall(8,8));	
+			cont.addWall(new Wall(9,9));
+			cont.addWall(new Wall(8,9));	
+			cont.addWall(new Wall(0,9));	
+			cont.addWall(new Wall(1,9));
 		}
 		// TODO Auto-generated constructor stub
 	}
